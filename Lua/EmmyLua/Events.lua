b54bb5e9b45be13df3ca28886ad72e2a137c1836
Ketho <sghs17@outlook.com>
2020-06-10 15:18:36 +0200

    Sort events

--- Lua/EmmyLua/Events.lua
+++ Lua/EmmyLua/Events.lua
@@ -1,8 +1,10 @@
 function Emmy:GetEvents()
+	table.sort(APIDocumentation.events, function(a, b)
+		return a.LiteralName < b.LiteralName
+	end)
 	local tbl = {}
 	tinsert(tbl, "Event = {")
 	for _, event in ipairs(APIDocumentation.events) do
-		local system = event.System.Namespace or event.System.Name
 		local payload = event:GetPayloadString(true, false)
 		payload = #payload > 0 and format('"%s"', payload) or "false"
 		tinsert(tbl, format('\t%s = %s,', event.LiteralName, payload))
