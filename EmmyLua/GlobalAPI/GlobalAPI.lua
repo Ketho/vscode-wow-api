d125c8b63c062b4a9a305dd3d849d277b7868fd3
Ketho <sghs17@outlook.com>
2021-02-20 23:38:04 +0100

    Test CreateFrame

---
+++ EmmyLua/GlobalAPI/GlobalAPI.lua
@@ -0,0 +1,10 @@
+---@generic Frame
+---@param frameType `Frame` | '"Frame"' | '"Button"' | '"PlayerModel"'
+---@param name string
+---@param parent Frame
+---@param template string
+---@param id number
+---@return Frame table
+---[Documentation](https://wow.gamepedia.com/API_CreateFrame)
+function CreateFrame(frameType, name, parent, template, id)
+end
