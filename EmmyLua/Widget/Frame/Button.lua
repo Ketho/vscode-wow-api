5970b49ca0decceb5ec0ca9fa6029452e1e2d5a5
Ketho <sghs17@outlook.com>
2021-02-22 19:37:37 +0100

    Button, CheckButton

--- EmmyLua/Widget/Frame/Button.lua
+++ EmmyLua/Widget/Frame/Button.lua
@@ -1,9 +1,122 @@
 ---@class Button : Frame
 local Button = {}
 
-function Button:Enable() end
+---[Documentation](https://wow.gamepedia.com/API_Button_Click)
+function Button:Click() end
 
+---[Documentation](https://wow.gamepedia.com/API_Button_Disable)
 function Button:Disable() end
 
----@return boolean
+---[Documentation](https://wow.gamepedia.com/API_Button_Enable)
+function Button:Enable() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetButtonState)
+function Button:GetButtonState() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetDisabledFontObject)
+function Button:GetDisabledFontObject() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetDisabledTexture)
+function Button:GetDisabledTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetFontString)
+function Button:GetFontString() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetHighlightFontObject)
+function Button:GetHighlightFontObject() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetHighlightTexture)
+function Button:GetHighlightTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetMotionScriptsWhileDisabled)
+function Button:GetMotionScriptsWhileDisabled() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetNormalFontObject)
+function Button:GetNormalFontObject() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetNormalTexture)
+function Button:GetNormalTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetPushedTextOffset)
+function Button:GetPushedTextOffset() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetPushedTexture)
+function Button:GetPushedTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetText)
+function Button:GetText() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetTextHeight)
+function Button:GetTextHeight() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_GetTextWidth)
+function Button:GetTextWidth() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_IsEnabled)
 function Button:IsEnabled() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_LockHighlight)
+function Button:LockHighlight() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_RegisterForClicks)
+function Button:RegisterForClicks() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_RegisterForMouse)
+function Button:RegisterForMouse() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetButtonState)
+function Button:SetButtonState() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetDisabledAtlas)
+function Button:SetDisabledAtlas() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetDisabledFontObject)
+function Button:SetDisabledFontObject() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetDisabledTexture)
+function Button:SetDisabledTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetEnabled)
+function Button:SetEnabled() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetFontString)
+function Button:SetFontString() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetFormattedText)
+function Button:SetFormattedText() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetHighlightAtlas)
+function Button:SetHighlightAtlas() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetHighlightFontObject)
+function Button:SetHighlightFontObject() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetHighlightTexture)
+function Button:SetHighlightTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetMotionScriptsWhileDisabled)
+function Button:SetMotionScriptsWhileDisabled() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetNormalAtlas)
+function Button:SetNormalAtlas() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetNormalFontObject)
+function Button:SetNormalFontObject() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetNormalTexture)
+function Button:SetNormalTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetPushedAtlas)
+function Button:SetPushedAtlas() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetPushedTextOffset)
+function Button:SetPushedTextOffset() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetPushedTexture)
+function Button:SetPushedTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_SetText)
+function Button:SetText() end
+
+---[Documentation](https://wow.gamepedia.com/API_Button_UnlockHighlight)
+function Button:UnlockHighlight() end
---
+++ EmmyLua/Widget/Frame/CheckButton.lua
@@ -0,0 +1,20 @@
+---@class CheckButton : Button
+local CheckButton = {}
+
+---[Documentation](https://wow.gamepedia.com/API_CheckButton_GetChecked)
+function CheckButton:GetChecked() end
+
+---[Documentation](https://wow.gamepedia.com/API_CheckButton_GetCheckedTexture)
+function CheckButton:GetCheckedTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_CheckButton_GetDisabledCheckedTexture)
+function CheckButton:GetDisabledCheckedTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_CheckButton_SetChecked)
+function CheckButton:SetChecked() end
+
+---[Documentation](https://wow.gamepedia.com/API_CheckButton_SetCheckedTexture)
+function CheckButton:SetCheckedTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_CheckButton_SetDisabledCheckedTexture)
+function CheckButton:SetDisabledCheckedTexture() end
