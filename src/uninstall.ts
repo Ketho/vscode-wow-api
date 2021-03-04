461c773d5a5c637f801b4a32ca211e4695cd55e9
Ketho <sghs17@outlook.com>
2021-03-04 18:36:46 +0100

    Try fixing uninstall hook

--- src/uninstall.ts
+++ src/uninstall.ts
@@ -1,7 +1,7 @@
 // extensions only seem to be removed from disk after uninstalling and restarting twice
 // so we should remove our emmylua folder from external libraries
 // also to avoid littering it with 0.0.3, 0.0.4 paths, etc
-let setLuaLibrary = require("./extension")
+let setLuaLibrary = require("./extension").setLuaLibrary
 setLuaLibrary(undefined) // does this ever get called?
 
 // not sure if there is a "Extension disable hook"
