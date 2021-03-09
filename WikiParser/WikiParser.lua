bcc10edd3dac83696c3990af6505b84a362e35c4
Ketho <sghs17@outlook.com>
2021-03-09 09:43:00 +0100

    Validate function signature arguments

--- WikiParser/WikiParser.lua
+++ WikiParser/WikiParser.lua
@@ -32,14 +32,15 @@ function m:ParsePages(limit)
 		info.idx = k
 		info.apiName = v.title:match("API (.+)"):gsub(" ", "_")
 		info.params = {arguments = {}, returns = {}}
+		info.signature = {}
 		local wikiText = v.revision.text[1]
 		for line in wikiText:gmatch("[^\r\n]+") do
 			local lineLower = line:lower()
 			-- signature
-			if not info.foundSignature and line:find("^%s") and line:find("%(") then
+			if not info.signature.found and line:find("^%s") and line:find("%(") then
 				if self:ParseSignature(line, info) then
-					info.foundSignature = true
-					info.signature = line
+					info.signature.found = true
+					info.signature.line = line
 				end
 			end
 			-- update current section
@@ -50,13 +51,16 @@ function m:ParsePages(limit)
 				if name then
 					local paramTbl = info.params[info.section]
 					if paramTbl then
-						table.insert(paramTbl, {name, valueType})
+						table.insert(paramTbl, {
+							name = name,
+							type = valueType
+						})
 					end
 				end
 			end
 		end
-		API[info.apiName] = info
-		self:PrintApi(info.apiName)
+		API[info.idx] = info
+		self:PrintApi(info)
 
 		if limit and k >= limit then
 			break
@@ -66,13 +70,18 @@ end
 
 function m:ParseSignature(line, info)
 	local foundSignature
+	info.signature = {}
 	-- match everything that is not a space up to the left bracket
-	local sigName, sigArgs = line:match("^%s.-([^%s]-)%((.-)%)")
+	local sigRets, sigName, sigArgs = line:match("^%s(.-)([^%s]-)%((.-)%)")
 	if sigName then
-		--print(sigName, sigArgs)
 		foundSignature = true
-		if info.apiName ~= sigName then
-			--print("signature error:", info.apiName, sigName)
+		info.signature.name = sigName
+		if #sigArgs > 0 then
+			info.signature.args = {}
+			-- lazy way instead of splitting string by comma
+			for s in string.gmatch(sigArgs, "%w+") do
+				table.insert(info.signature.args, s)
+			end
 		end
 	end
 	return foundSignature
@@ -91,27 +100,44 @@ end
 
 local function printApiParam(t)
 	for _, v in pairs(t) do
-		print("\t\t"..v[1]..": "..v[2])
+		print("\t\t"..v.name..": "..v.type)
 	end
 end
 
-function m:PrintApi(name)
-	local info = API[name]
-	print(info.idx.." "..name)
-	print(info.signature)
-	local args = info.params.arguments
-	local rets = info.params.returns
-	if next(args) then
+function m:PrintApi(info)
+	print(info.idx.." "..info.apiName)
+	print(info.signature.line)
+	if #info.params.arguments > 0 then
 		print("\targuments")
-		printApiParam(args)
+		printApiParam(info.params.arguments)
 	end
-	if next(rets) then
+	if #info.params.returns > 0 then
 		print("\treturns")
-		printApiParam(rets)
+		printApiParam(info.params.returns)
 	end
 	print()
 end
 
+function m:ValidateApi()
+	for apiIdx, info in pairs(API) do
+		if not info.signature.name then
+			print(string.format("%d:%s - signature not found", apiIdx, info.apiName))
+		elseif #info.signature.name == 0 then
+			print(string.format("%d:%s - signature function name not found", apiIdx, info.apiName))
+		elseif info.apiName ~= info.signature.name then
+			print(string.format("%d:%s - signature function name does not match: %s", apiIdx, info.apiName, info.signature.name))
+		end
+		for i, param in pairs(info.params.arguments) do
+			if not info.signature.args then
+				print(string.format("%d:%s - could not find signature arguments", apiIdx, info.apiName))
+			elseif info.signature.args[i] ~= param.name then
+				print(string.format("%d:%s - argument does not match: %s, %s", apiIdx, info.apiName, info.signature[i], param.name))
+			end
+		end
+	end
+end
+
 m:ParsePages(100)
+m:ValidateApi()
 
 print("done")
