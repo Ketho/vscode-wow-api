6d9d442a57c45b9ef062854232ee609120fa2f9c
Ketho <sghs17@outlook.com>
2021-03-14 12:59:44 +0100

    First parse without throwing errors

--- WikiParser/WikiParser.lua
+++ WikiParser/WikiParser.lua
@@ -1,6 +1,7 @@
+-- fuck wikis for documentation
 local xml2lua = require("xml2lua")
 local handler = require("xmlhandler.tree")
-local path = [[WikiParser\Wowpedia-20210306033305.xml]]
+local path = [[WikiParser\Wowpedia-20210314114900.xml]]
 
 -- parse xml from file
 local xmlstr = xml2lua.loadFile(path)
@@ -15,11 +16,11 @@ local validTypes = {
 	["function"] = true,
 }
 
-local function removeFormatting(s)
-	return s:gsub("[%[%]]", "")
-end
+local validParamSections = {
+	arguments = true,
+	returns = true,
+}
 
-local API = {}
 local m = {}
 
 local function isSearchResult(options, info)
@@ -35,27 +36,32 @@ end
 
 function m:ParsePages(options)
 	for k, v in pairs(handler.root.mediawiki.page) do
-		print(v.title)
+		-- print(v.title) --debug
 		local info = {}
 		info.idx = k
 		info.apiName = v.title:match("API (.+)"):gsub(" ", "_")
 		if isSearchResult(options or {}, info) then
 			info.params = {arguments = {}, returns = {}}
 			info.signature = {}
+			info.signature.lines = {}
+			local parsingCodeBlock
 			local wikiText = v.revision.text[1]
 			for line in wikiText:gmatch("[^\r\n]+") do
 				local lineLower = line:lower()
-				-- signature
-				if not info.signature.found and line:find("^%s") and line:find("%(") then
-					if self:ParseSignature(line, info) then
-						info.signature.found = true
-						info.signature.line = line
-					end
+				local isCodeBlock = line:find("^%s")
+				-- assume the first code block will always be the signature
+				-- signatures can be multi-line
+				if isCodeBlock and not info.signature.found then
+					parsingCodeBlock = true
+					table.insert(info.signature.lines, line)
+				elseif not isCodeBlock and parsingCodeBlock then
+					info.signature.found = true
+					self:ParseSignature(info.signature.lines, info)
 				end
 				-- update current section
 				info.section = lineLower:match("==%s?(.-)%s?==") or info.section
 				-- look for params
-				if line:find("^;") and line:find(":") then
+				if line:find("^:?;") and line:find(":") and validParamSections[info.section] then
 					local name, valueType = self:ParseParam(line, info)
 					if name then
 						local paramTbl = info.params[info.section]
@@ -68,19 +74,30 @@ function m:ParsePages(options)
 					end
 				end
 			end
-			API[k] = info
-			self:PrintApi(info)
+			-- self:PrintApi(info)
+			self:ValidateApi(info)
 		end
 	end
 end
 
-function m:ParseSignature(line, info)
-	local foundSignature
-	info.signature = {}
-	-- match everything that is not a space up to the left bracket
-	local sigRets, sigName, sigArgs = line:match("^%s(.-)([^%s]-)%((.-)%)")
+function m:ParseSignature(lines, info)
+	local isMultiline = (#lines > 1)
+	local sigRets, sigName, sigArgs
+	-- get signature parts
+	if isMultiline then
+		for _, line in pairs(lines) do
+			if line:find("=") then
+				sigName, sigArgs = line:match("^%s([^%s]-)%((.-)%)")
+			else
+				sigRets = line
+			end
+		end
+	else
+		-- match everything that is not a space up to the left bracket
+		sigRets, sigName, sigArgs = lines[1]:match("^%s(.-)([^%s]-)%((.-)%)")
+	end
+	-- parse signature
 	if sigName then
-		foundSignature = true
 		info.signature.name = sigName
 		if #sigArgs > 0 then
 			info.signature.arguments = {}
@@ -89,22 +106,24 @@ function m:ParseSignature(line, info)
 				table.insert(info.signature.arguments, s)
 			end
 		end
-		if #sigRets > 0 then
-			info.signature.returns = {}
-			for s in string.gmatch(sigRets, "%w+") do
-				table.insert(info.signature.returns, s)
-			end
+	end
+	if sigRets and #sigRets > 0 then
+		info.signature.returns = {}
+		for s in string.gmatch(sigRets, "%w+") do
+			table.insert(info.signature.returns, s)
 		end
 	end
-	return foundSignature
 end
 
 function m:ParseParam(line, info)
-	line = line:match("(.-)%-") or line -- remove any comment text
-	local name, valueType = line:match(";%s?(.-)%s?:%s?(%S+)")
-	-- match type from {{api|t=t|number}}
+	line = line:match("(.-)%s?%-") or line -- remove any comment text
+	-- not sure if we should use patterns to handle multiple formats
+	local name, valueType = line:match(";%d*%.?%s?(.-)%s?:%s?(.+)")
+	-- <font color="#ecbc2a">number</font>
+	valueType = valueType:match("(%w+)</font>") or valueType
+	-- {{api|t=t|number}}
 	valueType = valueType:match("(%w+)}}") or valueType
-	name = removeFormatting(name)
+	name = name:match("(%w+)%]%]") or name
 	valueType = valueType:lower()
 	return name, valueType
 end
@@ -117,7 +136,9 @@ end
 
 function m:PrintApi(info)
 	print(info.idx.." "..info.apiName)
-	print(info.signature.line)
+	for _, line in pairs(info.signature.lines) do
+		print(line)
+	end
 	if #info.params.arguments > 0 then
 		print("\tparam arguments")
 		printApiParam(info.params.arguments)
@@ -141,41 +162,38 @@ function m:PrintApi(info)
 	print()
 end
 
-function m:ValidateApi()
-	for apiIdx, info in pairs(API) do
-		if not info.signature.name then
-			print(string.format("%d:%s - signature not found", apiIdx, info.apiName))
-		elseif #info.signature.name == 0 then
-			print(string.format("%d:%s - signature function name not found", apiIdx, info.apiName))
-		elseif info.apiName ~= info.signature.name then
-			print(string.format("%d:%s - signature function name does not match: %s", apiIdx, info.apiName, info.signature.name))
+function m:ValidateApi(info)
+	if not info.signature.name then
+		print(string.format("%d:%s - signature not found", info.idx, info.apiName))
+	elseif #info.signature.name == 0 then
+		print(string.format("%d:%s - signature function name not found", info.idx, info.apiName))
+	elseif info.apiName ~= info.signature.name then
+		print(string.format("%d:%s - signature function name does not match: %s", info.idx, info.apiName, info.signature.name))
+	end
+	for i, param in pairs(info.params.arguments) do
+		if not info.signature.arguments then
+			print(string.format("%d:%s - could not find signature arguments", info.idx, info.apiName))
+		elseif info.signature.arguments[i] ~= param.name then
+			print(string.format("%d:%s - argument does not match: %s, %s", info.idx, info.apiName, info.signature[i], param.name))
 		end
-		for i, param in pairs(info.params.arguments) do
-			if not info.signature.arguments then
-				print(string.format("%d:%s - could not find signature arguments", apiIdx, info.apiName))
-			elseif info.signature.arguments[i] ~= param.name then
-				print(string.format("%d:%s - argument does not match: %s, %s", apiIdx, info.apiName, info.signature[i], param.name))
-			end
-			if not validTypes[param.type] then
-				print(string.format("%d:%s - argument type is not valid: %s, %s", apiIdx, info.apiName, param.name, param.type))
-			end
+		if not validTypes[param.type] then
+			print(string.format("%d:%s - argument type is not valid: %s, %s", info.idx, info.apiName, param.name, param.type))
 		end
-		for i, param in pairs(info.params.returns) do
-			if not info.signature.returns then
-				print(string.format("%d:%s - could not find signature returns", apiIdx, info.apiName))
-			elseif info.signature.returns[i] ~= param.name then
-				print(string.format("%d:%s - return value does not match: %s, %s", apiIdx, info.apiName, info.signature[i], param.name))
-			end
-			if not validTypes[param.type] then
-				print(string.format("%d:%s - return type is not valid: %s, %s", apiIdx, info.apiName, param.name, param.type))
-			end
+	end
+	for i, param in pairs(info.params.returns) do
+		if not info.signature.returns then
+			print(string.format("%d:%s - could not find signature returns", info.idx, info.apiName))
+		elseif info.signature.returns[i] ~= param.name then
+			print(string.format("%d:%s - return value does not match: %s, %s", info.idx, info.apiName, info.signature[i], param.name))
+		end
+		if not validTypes[param.type] then
+			print(string.format("%d:%s - return type is not valid: %s, %s", info.idx, info.apiName, param.name, param.type))
 		end
 	end
 end
 
 m:ParsePages()
--- m:ParsePages({range = {30, 40}})
+-- m:ParsePages({range = {844, 850}})
 -- m:ParsePages({name = "BNConnected"})
-m:ValidateApi()
 
 print("done")
