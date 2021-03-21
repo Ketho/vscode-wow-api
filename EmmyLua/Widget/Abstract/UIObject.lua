04ace8f20ea9eed1a915cb50ff8efcb9d809e836
Ketho <sghs17@outlook.com>
2021-03-21 21:01:11 +0100

    UIObject descriptions

--- EmmyLua/Widget/Abstract/UIObject.lua
+++ EmmyLua/Widget/Abstract/UIObject.lua
@@ -1,14 +1,17 @@
 ---@class UIObject
 local UIObject = {}
 
+--- Returns the widget object's name
 ---@return string name
 ---[Documentation](https://wow.gamepedia.com/API_UIObject_GetName)
 function UIObject:GetName() end
 
+--- Returns the object's widget type
 ---@return string objectType
 ---[Documentation](https://wow.gamepedia.com/API_UIObject_GetObjectType)
 function UIObject:GetObjectType() end
 
+--- Returns whether the object belongs to a given widget type
 ---@param type string
 ---@return boolean isType
 ---[Documentation](https://wow.gamepedia.com/API_UIObject_IsObjectType)
@@ -19,18 +22,22 @@ function UIObject:IsObjectType(type) end
 ---[Documentation](https://wow.gamepedia.com/UIOBJECT_ParentedObject)
 local ParentedObject = {}
 
+--- Returns the widget object's debug name
 ---@return string debugName
 ---[Documentation](https://wow.gamepedia.com/API_ParentedObject_GetDebugName)
 function ParentedObject:GetDebugName() end
 
+--- Returns the widget's parent object
 ---@return ParentedObject parent
 ---[Documentation](https://wow.gamepedia.com/API_ParentedObject_GetParent)
 function ParentedObject:GetParent() end
 
+--- Returns if this widget's methods may only be called from secure execution paths
 ---@return boolean isForbidden
 ---[Documentation](https://wow.gamepedia.com/API_ParentedObject_IsForbidden)
 function ParentedObject:IsForbidden() end
 
+--- Sets the widget to be forbidden for insecure code
 ---@param forbidden boolean
 ---[Documentation](https://wow.gamepedia.com/API_ParentedObject_SetForbidden)
 function ParentedObject:SetForbidden(forbidden) end
