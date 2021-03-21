cf525c468a62059f355d95b6d16b3c15264cde50
Ketho <sghs17@outlook.com>
2021-03-21 21:28:33 +0100

    Added GetUnitName (close #4)

---
+++ EmmyLua/FrameXML/UnitFrame.lua
@@ -0,0 +1,6 @@
+--- Returns the name and optionally the realm of the unit.
+---@param unit string
+---@param showServerName? boolean
+---@return string name
+---[FrameXML](https://www.townlong-yak.com/framexml/go/GetUnitName)
+function GetUnitName(unit, showServerName) end
