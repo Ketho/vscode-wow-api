d87981440a3224f3d3a0f104fc68056e5e44d288
Ketho <sghs17@outlook.com>
2021-03-25 18:26:07 +0100

    Added RAID_CLASS_COLORS with colorStr field

---
+++ EmmyLua/SharedXML/ColorUtil.lua
@@ -0,0 +1,19 @@
+---@class ColorMixinRCC : ColorMixin
+---@field colorStr string
+local ColorMixin = {}
+
+---@type table<string, ColorMixinRCC>
+RAID_CLASS_COLORS = {
+	WARRIOR = {},
+	PALADIN = {},
+	HUNTER = {},
+	ROGUE = {},
+	PRIEST = {},
+	DEATHKNIGHT = {},
+	SHAMAN = {},
+	MAGE = {},
+	WARLOCK = {},
+	MONK = {},
+	DRUID = {},
+	DEMONHUNTER = {},
+}
