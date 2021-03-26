60cf02fdd79717bfe987b7ba7a00043b43920e8a
Nightwarden24 <CodeNightwarden24@gmail.com>
2021-03-26 18:03:53 +0300

    Add EasingUtil.lua to SharedXML

---
+++ EmmyLua/SharedXML/EasingUtil.lua
@@ -0,0 +1,62 @@
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+EasingUtil = {}
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.InQuadratic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.OutQuadratic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.InOutQuadratic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.InCubic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.OutCubic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.InOutCubic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.InQuartic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.OutQuartic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.InOutQuartic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.InQuintic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.OutQuintic(percent) end
+
+---@param percent number Value from 0.0 to 1.0
+---@return number
+---[FrameXML](https://www.townlong-yak.com/framexml/live/EasingUtil.lua)
+function EasingUtil.InOutQuintic(percent) end
