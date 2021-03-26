c93c3905dd88aa0ebc5e4882ce2d5ea115d14c2a
Ketho <sghs17@outlook.com>
2021-03-26 20:29:00 +0100

    Basic StatusBar definitions

---
+++ EmmyLua/Widget/Frame/StatusBar.lua
@@ -0,0 +1,80 @@
+---@class StatusBar : Frame
+local StatusBar = {}
+
+---@param scriptType ScriptStatusBar
+---@param bindingType LE_SCRIPT_BINDING_TYPE
+---@return function handler
+---[Documentation](https://wow.gamepedia.com/API_ScriptObject_GetScript)
+function StatusBar:GetScript(scriptType, bindingType) end
+
+---@param scriptType ScriptButton
+---@return boolean hasScript
+---[Documentation](https://wow.gamepedia.com/API_ScriptObject_HasScript)
+function StatusBar:HasScript(scriptType) end
+
+---@param scriptType ScriptButton
+---@param handler function
+---@param bindingType LE_SCRIPT_BINDING_TYPE
+---@return boolean success
+---[Documentation](https://wow.gamepedia.com/API_ScriptObject_HookScript)
+function StatusBar:HookScript(scriptType, handler, bindingType) end
+
+---@param scriptType ScriptButton
+---@param handler function
+---[Documentation](https://wow.gamepedia.com/API_ScriptObject_SetScript)
+function StatusBar:SetScript(scriptType, handler) end
+
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_GetFillStyle)
+function StatusBar:GetFillStyle() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_GetMinMaxValues)
+function StatusBar:GetMinMaxValues() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_GetOrientation)
+function StatusBar:GetOrientation() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_GetReverseFill)
+function StatusBar:GetReverseFill() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_GetRotatesTexture)
+function StatusBar:GetRotatesTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_GetStatusBarAtlas)
+function StatusBar:GetStatusBarAtlas() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_GetStatusBarColor)
+function StatusBar:GetStatusBarColor() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_GetStatusBarTexture)
+function StatusBar:GetStatusBarTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_GetValue)
+function StatusBar:GetValue() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_SetFillStyle)
+function StatusBar:SetFillStyle(style) end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_SetMinMaxValues)
+function StatusBar:SetMinMaxValues(min, max) end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_SetOrientation)
+function StatusBar:SetOrientation(orientation) end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_SetReverseFill)
+function StatusBar:SetReverseFill(state) end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_SetRotatesTexture)
+function StatusBar:SetRotatesTexture() end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_SetStatusBarAtlas)
+function StatusBar:SetStatusBarAtlas(atlasName) end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_SetStatusBarColor)
+function StatusBar:SetStatusBarColor(r, g, b, alpha) end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_SetStatusBarTexture)
+function StatusBar:SetStatusBarTexture(texture_or_fileDataIDbool, layer, subLayer) end
+
+---[Documentation](https://wow.gamepedia.com/API_StatusBar_SetValue)
+function StatusBar:SetValue(value) end
