86934dfa258d8ccf98f445a5a19986d907a3c7b4
Ketho <sghs17@outlook.com>
2021-04-21 03:03:58 +0200

    Add Font widget

---
+++ EmmyLua/Widget/Font.lua
@@ -0,0 +1,12 @@
+---@class Font : FontInstance
+---[Documentation](https://wow.gamepedia.com/UIOBJECT_Font)
+local Font = {}
+
+---[Documentation](https://wow.gamepedia.com/API_Font_CopyFontObject)
+function Font:CopyFontObject(otherFont) end
+
+---[Documentation](https://wow.gamepedia.com/API_Font_GetAlpha)
+function Font:GetAlpha() end
+
+---[Documentation](https://wow.gamepedia.com/API_Font_SetAlpha)
+function Font:SetAlpha(alpha) end
