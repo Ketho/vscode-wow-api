cb45716b52e513ea1804d56df5c7056205e7f6b1
Ketho <sghs17@outlook.com>
2021-05-03 16:40:50 +0200

    Update C_TimerAugment.lua

--- EmmyLua/SharedXML/C_TimerAugment.lua
+++ EmmyLua/SharedXML/C_TimerAugment.lua
@@ -1,29 +1,26 @@
 ---@class Ticker
----@field _remainingIterations number
----@field _callback function
----@field _cancelled boolean|nil
 local TickerPrototype = {}
 
+--- Creates and starts a ticker that calls callback every duration seconds for N iterations.
+--- If iterations is nil, the ticker will loop until cancelled.
 ---@param duration number
 ---@param callback function
----@param iterations number
+---@param iterations? number
 ---@return Ticker
----Creates and starts a ticker that calls callback every duration seconds for N iterations.
----
 ---[FrameXML](https://www.townlong-yak.com/framexml/live/C_TimerAugment.lua)
 function C_Timer.NewTicker(duration, callback, iterations) end
 
+--- Creates and starts a cancellable timer that calls callback after duration seconds.
 ---@param duration number
 ---@param callback function
 ---@return Ticker
----Creates and starts a cancellable timer that calls callback after duration seconds.
----
 ---[FrameXML](https://www.townlong-yak.com/framexml/live/C_TimerAugment.lua)
 function C_Timer.NewTimer(duration, callback) end
 
----[FrameXML](https://www.townlong-yak.com/framexml/live/C_TimerAugment.lua)
+--- Cancels a ticker or timer. May be safely called within the ticker's callback in which
+--- case the ticker simply won't be started again.
+--- Cancel is guaranteed to be idempotent.
 function TickerPrototype:Cancel() end
 
 ---@return boolean
----[FrameXML](https://www.townlong-yak.com/framexml/live/C_TimerAugment.lua)
 function TickerPrototype:IsCancelled() end
