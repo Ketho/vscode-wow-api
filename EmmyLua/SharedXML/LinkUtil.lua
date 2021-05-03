e4751425dae537acb5bff8f2dcf75abf8e31b07c
Ketho <sghs17@outlook.com>
2021-05-03 17:05:22 +0200

    Update LinkUtil.lua

--- EmmyLua/SharedXML/LinkUtil.lua
+++ EmmyLua/SharedXML/LinkUtil.lua
@@ -1,35 +1,35 @@
 ---@param linkString string
 ---@return boolean containsHyperlink
----@return string|nil preString
----@return string|nil hyperlinkString
----@return string|nil postString
+---@return string? preString
+---@return string? hyperlinkString
+---@return string? postString
 ---[FrameXML](https://www.townlong-yak.com/framexml/go/ExtractHyperlinkString)
 function ExtractHyperlinkString(linkString) end
 
 ---@param link string
----@return string|nil linkType
----@return string|nil linkData
+---@return string? linkType
+---@return string? linkData
 ---[FrameXML](https://www.townlong-yak.com/framexml/go/ExtractLinkData)
 function ExtractLinkData(link) end
 
 ---@param linkString string
----@return string|nil
+---@return string?
 ---[FrameXML](https://www.townlong-yak.com/framexml/go/ExtractQuestRewardID)
 function ExtractQuestRewardID(linkString) end
 
 ---@param link string
----@return number|nil itemID
----@return string|nil strippedItemLink
+---@return number? itemID
+---@return string? strippedItemLink
 ---[FrameXML](https://www.townlong-yak.com/framexml/go/GetItemInfoFromHyperlink)
 function GetItemInfoFromHyperlink(link) end
 
 ---@param link string
----@return number|nil achievementID
----@return boolean|nil complete
+---@return number? achievementID
+---@return boolean? complete
 ---[FrameXML](https://www.townlong-yak.com/framexml/go/GetAchievementInfoFromHyperlink)
 function GetAchievementInfoFromHyperlink(link) end
 
----@param self any
+---@param self? object used for discarding when called from blizzard xml
 ---@param link string
 ---[FrameXML](https://www.townlong-yak.com/framexml/go/GetURLIndexAndLoadURL)
 function GetURLIndexAndLoadURL(self, link) end
