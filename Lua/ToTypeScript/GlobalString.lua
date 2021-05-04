d6fa5d7f66ba8492fee7640b43187bb246d8777a
Ketho <sghs17@outlook.com>
2021-05-04 07:37:04 +0200

    Use raw strings for globalstrings (#9)

--- Lua/ToTypeScript/GlobalString.lua
+++ Lua/ToTypeScript/GlobalString.lua
@@ -1,7 +1,4 @@
-local path = "Lua/Data/cache/GlobalStrings.lua"
-local url = "https://raw.githubusercontent.com/Ketho/BlizzardInterfaceResources/live/Resources/GlobalStrings.lua"
-Util:CacheFile(path, url)
-local file = io.open(path)
+local parser = require "Lua/Util/wowtoolsparser"
 
 local pre = [[interface GlobalStringInterface {
 	[key: string]: string
@@ -10,13 +7,41 @@ local pre = [[interface GlobalStringInterface {
 export const data: GlobalStringInterface = {
 ]]
 
+local slashStrings = {
+	KEY_BACKSLASH = function(s) return s:sub(1, 2) == "9." end, -- broken in 9.1.0
+	CHATLOGENABLED = function(s) return s:sub(1, 3) == "9.0" end, -- broken in 9.0.5
+	--COMBATLOGENABLED = true,
+}
+
+local function IsValidTableKey(s)
+	return not s:find("-") and not s:find("^%d")
+end
+
 local function ToTypeScript()
+	local globalstrings, usedBuild = parser.ReadCSV("globalstrings", {header = true, build = "9.0.5"})
+	local stringsTable = {}
+	for line in globalstrings:lines() do
+		local flags = tonumber(line.Flags)
+		if flags and flags&0x1 > 0 then
+			table.insert(stringsTable, {
+				BaseTag = line.BaseTag,
+				TagText = line.TagText_lang
+			})
+		end
+	end
+	table.sort(stringsTable, function(a, b)
+		return a.BaseTag < b.BaseTag
+	end)
 	local t = {}
-	for line in file:lines() do
-		local globalstring, value = line:match("(.-) = (.+);")
-		-- don't bother about globals that need to be indexed through _G
-		if globalstring and not globalstring:find("^_G") then
-			tinsert(t, format("\t%s: %s,", globalstring, value))
+	local fs = '\t%s: String.raw`%s`,'
+	for _, tbl in pairs(stringsTable) do
+		local key, value = tbl.BaseTag, tbl.TagText
+		value = value:gsub('\\32', ' ') -- space char
+		if slashStrings[key] and slashStrings[key](usedBuild) then
+			value = value:gsub("\\", "\\\\")
+		end
+		if IsValidTableKey(key) then
+			tinsert(t, fs:format(key, value))
 		end
 	end
 	tinsert(t, "}\n")
