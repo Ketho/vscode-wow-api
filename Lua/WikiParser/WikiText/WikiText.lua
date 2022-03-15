c7bbdfcf4e0e634f490a0f2692cf5c250f8bd1f7
Ketho <sghs17@outlook.com>
2022-03-15 02:28:28 +0100

    Fix luacheck

--- Lua/WikiParser/WikiText/WikiText.lua
+++ Lua/WikiParser/WikiText/WikiText.lua
@@ -23,13 +23,10 @@ function m:ReplaceHtml(text)
 end
 
 function m:GetWikitext(isRetail)
-	local file = io.open(INPUT)
-	local text = file:read("a")
-	file:close()
 	local xmlstr = xml2lua.loadFile(INPUT)
 	local parser = xml2lua.parser(handler)
 	parser:parse(xmlstr)
-	text = handler.root.mediawiki.page.revision.text[1]
+	local text = handler.root.mediawiki.page.revision.text[1]
 	text = self:ReplaceHtml(text)
 	if isRetail then
 		local str_start = text:find("== API Reference ==")
