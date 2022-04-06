e093ebdee33dd9486bbcb52d9fea20bc93e420ce
wutname1 <jeremy12@gmail.com>
2022-04-06 12:25:04 -0500

    AceConsole Documentation links

--- EmmyLua/API/Libraries/Ace3/AceConsole-3.0.lua
+++ EmmyLua/API/Libraries/Ace3/AceConsole-3.0.lua
@@ -11,12 +11,14 @@
 ---@field embeds table ---table containing objects AceConsole is embedded in.
 ---@field commands table ---table containing commands registered
 ---@field weakcommands table ---table containing self, command => func references for weak commands that don't persist through enable/disable
+---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0)
 local AceConsole = {}
 
 ---Print to DEFAULT_CHAT_FRAME or given ChatFrame (anything with an .AddMessage function)
 ---@param chatframe Frame? Custom ChatFrame to print to (or any frame with an .AddMessage function)
 ---@param ... any List of any values to be printed
 ---@overload fun(...: any)
+---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0#title-3)
 function AceConsole:Print(chatframe, ...) end
 
 
@@ -25,6 +27,7 @@ function AceConsole:Print(chatframe, ...) end
 ---@param format string same syntax as standard Lua format()
 ---@param ...? any Arguments to the format string
 ---@overload fun(format: string)
+---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0#title-4)
 function AceConsole:Printf(chatframe, format, ...) end
 
 ---Register a simple chat command
@@ -32,14 +35,17 @@ function AceConsole:Printf(chatframe, format, ...) end
 ---@param func|string function Function to call when the slash command is being used (funcref or methodname)
 ---@param persist? boolean if false, the command will be soft disabled/enabled when aceconsole is used as a mixin (default: true)
 ---@return boolean -- true if successful
+---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0#title-5)
 function AceConsole:RegisterChatCommand( command, func, persist) end
 
 ---Unregister a chatcommand
 ---@param command string Chat command to be unregistered WITHOUT leading "/"
+---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0#title-6)
 function AceConsole:UnregisterChatCommand( command )end
 
 ---Get an iterator over all Chat Commands registered with AceConsole
 ---@return table -- Iterator (pairs) over all commands
+---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0#title-2)
 function AceConsole:IterateChatCommands() end
 
 ---Retreive one or more space-separated arguments from a string.
@@ -49,4 +55,5 @@ function AceConsole:IterateChatCommands() end
 ---@param startpos? number Where in the string to start scanning (default  1)
 ---@return table
 ---Missing arguments will be returned as nils. 'nextposition' is returned as 1e9 at the end of the string.
+---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0#title-1)
 function AceConsole:GetArgs(str, numargs, startpos) end
