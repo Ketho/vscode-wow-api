e7426b73ae27253f66a31cb5010403143ba8ac9d
wutname1 <jeremy12@gmail.com>
2022-04-06 12:23:29 -0500

    Add AceConsole

---
+++ EmmyLua/API/Libraries/Ace3/AceConsole-3.0.lua
@@ -0,0 +1,52 @@
+---**AceConsole-3.0** provides registration facilities for slash commands.
+---You can register slash commands to your custom functions and use the `GetArgs` function to parse them
+---to your addons individual needs.
+---
+---**AceConsole-3.0** can be embeded into your addon, either explicitly by calling AceConsole:Embed(MyAddon) or by
+---specifying it as an embeded library in your AceAddon. All functions will be available on your addon object
+---and can be accessed directly, without having to explicitly call AceConsole itself.\\
+---It is recommended to embed AceConsole, otherwise you'll have to specify a custom `self` on all calls you
+---make into AceConsole.
+---@class AceConsole-3.0
+---@field embeds table ---table containing objects AceConsole is embedded in.
+---@field commands table ---table containing commands registered
+---@field weakcommands table ---table containing self, command => func references for weak commands that don't persist through enable/disable
+local AceConsole = {}
+
+---Print to DEFAULT_CHAT_FRAME or given ChatFrame (anything with an .AddMessage function)
+---@param chatframe Frame? Custom ChatFrame to print to (or any frame with an .AddMessage function)
+---@param ... any List of any values to be printed
+---@overload fun(...: any)
+function AceConsole:Print(chatframe, ...) end
+
+
+---Formatted (using format()) print to DEFAULT_CHAT_FRAME or given ChatFrame (anything with an .AddMessage function)
+---@param chatframe? Frame Custom ChatFrame to print to (or any frame with an .AddMessage function)
+---@param format string same syntax as standard Lua format()
+---@param ...? any Arguments to the format string
+---@overload fun(format: string)
+function AceConsole:Printf(chatframe, format, ...) end
+
+---Register a simple chat command
+---@param command string Chat command to be registered WITHOUT leading "/"
+---@param func|string function Function to call when the slash command is being used (funcref or methodname)
+---@param persist? boolean if false, the command will be soft disabled/enabled when aceconsole is used as a mixin (default: true)
+---@return boolean -- true if successful
+function AceConsole:RegisterChatCommand( command, func, persist) end
+
+---Unregister a chatcommand
+---@param command string Chat command to be unregistered WITHOUT leading "/"
+function AceConsole:UnregisterChatCommand( command )end
+
+---Get an iterator over all Chat Commands registered with AceConsole
+---@return table -- Iterator (pairs) over all commands
+function AceConsole:IterateChatCommands() end
+
+---Retreive one or more space-separated arguments from a string.
+---Treats quoted strings and itemlinks as non-spaced.
+---@param str string The raw argument string
+---@param numargs? number How many arguments to get (default 1)
+---@param startpos? number Where in the string to start scanning (default  1)
+---@return table
+---Missing arguments will be returned as nils. 'nextposition' is returned as 1e9 at the end of the string.
+function AceConsole:GetArgs(str, numargs, startpos) end
