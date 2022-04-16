ebfa5f51dbd0020d6cc84a95554aa5240e3a3c9b
wutname1 <jeremy12@gmail.com>
2022-04-16 03:00:07 -0500

    First pass on AceConfig

---
+++ EmmyLua/API/Libraries/Ace3/AceConfig-3.0.lua
@@ -0,0 +1,59 @@
+---@class AceConfig-3.0
+local AceConfig = {}
+
+---Register a option table with the AceConfig registry.
+---You can supply a slash command (or a table of slash commands) to register with AceConfigCmd directly.
+---@paramsig appName, options [, slashcmd]
+---@param appName string The application name for the config table.
+---@param options table|function|AceConfig-3.0OptionsTable The option table (or a function to generate one on demand). [Options table documentation](http://www.wowace.com/addons/ace3/pages/ace-config-3-0-options-tables/)
+---@param slashcmd? string|table A slash command to register for the option table, or a table of slash commands.
+---```
+---local AceConfig = LibStub("AceConfig-3.0")
+---AceConfig:RegisterOptionsTable("MyAddon", myOptions, {"/myslash", "/my"})
+---```
+---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-config-3-0#title-1)
+function AceConfig:RegisterOptionsTable(appName, options, slashcmd) end
+
+---@alias Ace3Width
+---| '"double"'
+---| '"half"'
+---| '"full"'
+---| '"normal"'
+
+---@alias AceConfigTypes
+---| '"execute"'
+---| '"input"'
+---| '"toggle"'
+---| '"range"'
+---| '"select"'
+---| '"multiselect"'
+---| '"color"'
+---| '"keybinding"'
+---| '"header"'
+---| '"description"'
+---| '"group"'
+
+---@class AceConfig-3.0OptionsTable
+---@field name string|function Display name for the option
+---@field type AceConfigTypes Type of the option
+---@field desc? string|function description for the option (or nil for a self-describing name)
+---@field descStyle? string "inline" if you want the description to show below the option in a GUI (rather than as a tooltip). Currently only supported by AceGUI "Toggle".
+---@field order? number|string|function The order in the GUI that the option should be displayed in.
+---@field validate? string|function|false validate the input/value before setting it. return a string (error message) to indicate error.
+---@field confirm? string|function|boolean prompt for confirmation before changing a value if true display "name - desc", or contents of .confirmText if supplied.
+---@field confirmText? string text to display in the confirmation dialog
+---@field disabled? string|function|boolean
+---@field hidden? string|function|boolean
+---@field guiHidden? boolean
+---@field dialogHidden? boolean
+---@field dropdownHidden? boolean
+---@field cmdHidden? boolean
+---@field icon? string|function
+---@field iconCoords? table|string|function
+---@field handler? table
+---@field get? function getter function
+---@field set? function setter function
+---@field func? function
+local OptionsTable = {}
+OptionsTable.args = {} ---@type AceConfig-3.0OptionsTable
+OptionsTable.width = "normal" ---@type number|Ace3Width|nil If a number multiplier of the default width, ie. 0.5 equals "half", 2.0 equals "double"
