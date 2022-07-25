d1e812aabc5ae794b49caf7a09a1b26704b7d7d6
Ketho <sghs17@outlook.com>
2022-07-25 17:46:05 +0200

    Add manualstructures

---
+++ EmmyLua/API/Structures/ManualStructures.lua
@@ -0,0 +1,12 @@
+---@meta
+---[Documentation](https://wowpedia.fandom.com/wiki/Struct_FontInfo)
+---@class FontInfo
+---@field height number	
+---@field outline string
+---@field color table
+---@field shadow FontShadow
+
+---@class FontShadow
+---@field x number	
+---@field y number
+---@field color table
