759762091eb7fcb77e7fc9f1a0209141c88690d3
Ketho <sghs17@outlook.com>
2023-01-11 20:09:11 +0100

    Add EventRegistry

---
+++ EmmyLua/API/SharedXML/GlobalCallbackRegistry.lua
@@ -0,0 +1,29 @@
+---@meta
+---[Documentation](https://wowpedia.fandom.com/wiki/EventRegistry)
+EventRegistry = CreateFromMixins(CallbackRegistryMixin)
+
+-- function EventRegistry:OnLoad() end
+
+---@param frameEvent WowEvent
+---@param value number
+function EventRegistry:OnAttributeChanged(frameEvent, value) end
+
+---@param frameEvent WowEvent
+function EventRegistry:RegisterFrameEvent(frameEvent) end
+
+---@param frameEvent WowEvent
+function EventRegistry:UnregisterFrameEvent(frameEvent) end
+
+---@param frameEvent WowEvent
+---@return any owner
+function EventRegistry:RegisterFrameEventAndCallback(frameEvent, ...) end
+
+---@param frameEvent WowEvent
+---@return table
+function EventRegistry:RegisterFrameEventAndCallbackWithHandle(frameEvent, ...) end
+
+---@param frameEvent WowEvent
+function EventRegistry:UnregisterFrameEventAndCallback(frameEvent, ...) end
+
+---@return string
+function EventRegistry:GetEventCounts(...) end
