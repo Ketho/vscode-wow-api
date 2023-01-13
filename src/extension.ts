de2e9fe84ef525c38fcc75824ceb26ba767f5811
Ketho <sghs17@outlook.com>
2023-01-13 11:38:37 +0100

    Only fix "param-type-mismatch" warning on demand

--- src/extension.ts
+++ src/extension.ts
@@ -46,7 +46,6 @@ export function activate(context: vscode.ExtensionContext) {
 	setExternalLibrary("API", true);
 	const loadFrameXML = vscode.workspace.getConfiguration("wowAPI").get("emmyLua.loadFrameXML");
 	setExternalLibrary("Optional", loadFrameXML ? true : false);
-	updateConfigs();
 
 	const completion = vscode.languages.registerCompletionItemProvider(
 		"lua",
@@ -68,7 +67,6 @@ export function activate(context: vscode.ExtensionContext) {
 		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 		"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
 	);
-	onCustomCompletion();
 
 	const hover = vscode.languages.registerHoverProvider(
 		"lua",
@@ -110,18 +108,28 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	// automatically mark wow globals as defined if there is a language server warning
+	const config = vscode.workspace.getConfiguration("Lua");
+	const diag_globals: string[] = config.get("diagnostics.globals")!;
+	const diag_disable: string[] = config.get("diagnostics.disable")!;
+
 	vscode.languages.onDidChangeDiagnostics((event: vscode.DiagnosticChangeEvent) => {
-		const config = vscode.workspace.getConfiguration("Lua");
-		const globals: string[] | undefined = config.get("diagnostics.globals");
 		event.uris.forEach(function(uri) {
 			let diags = vscode.languages.getDiagnostics(uri);
 			diags.forEach(function(diag) {
-				if (diag.code === "undefined-global" && globals) {
-					let glob = diag.message.match("Undefined global `(.+)`\.");
-					if (glob && globals.indexOf(glob[1]) === -1 && wow_globals[glob[1]]) {
-						globals?.push(glob[1]);
-						config.update("diagnostics.globals", globals);
+				// automatically mark wow globals as defined if there is a language server warning
+				if (diag.code === "undefined-global") {
+					let glob = diag.message.match("`(.+)`");
+					if (glob && wow_globals[glob[1]] && !diag_globals.includes(glob[1])) {
+						diag_globals.push(glob[1]);
+						config.update("diagnostics.globals", diag_globals);
+					}
+				}
+				// cannot seem to easily fix warnings like these in emmylua wow
+				// "Cannot assign `CheckButton|InterfaceOptionsCheckButtonTemplate` to parameter `string|Region`"
+				else if (diag.code === "param-type-mismatch") {
+					if (diag.message.includes("Template") && !diag_disable.includes("param-type-mismatch")) {
+						diag_disable.push("param-type-mismatch");
+						config.update("diagnostics.disable", diag_disable);
 					}
 				}
 			});
@@ -138,67 +146,30 @@ export function deactivate() {
 export function setExternalLibrary(folder: string, enable: boolean) {
 	// console.log("setExternalLibrary", folder, enable);
 	const extensionId = "ketho.wow-api";
-	const extensionPath = vscode.extensions.getExtension(extensionId)?.extensionPath;
+	const extensionPath = vscode.extensions.getExtension(extensionId)!.extensionPath;
 	// Use path.join to ensure the proper path seperators (\ for windows, / for anything else) are used.
-	const folderPath = path.join(extensionPath!, "EmmyLua", folder);
+	const folderPath = path.join(extensionPath, "EmmyLua", folder);
 	const config = vscode.workspace.getConfiguration("Lua");
-	const library: string[] | undefined = config.get("workspace.library");
-	if (library && extensionPath) {
-		// remove any older versions of our path e.g. "publisher.name-0.0.1"
-		for (let i = library.length-1; i >= 0; i--) {
-			const el = library[i];
-			const isSelfExtension = el.indexOf(extensionId) > -1;
-			const isCurrentVersion = el.indexOf(extensionPath) > -1;
-			if (isSelfExtension && !isCurrentVersion) {
-				library.splice(i, 1);
-			}
+	const library: string[] = config.get("workspace.library")!;
+	// remove any older versions of our path e.g. "publisher.name-0.0.1"
+	for (let i = library.length-1; i >= 0; i--) {
+		const el = library[i];
+		const isSelfExtension = el.includes(extensionId);
+		const isCurrentVersion = el.includes(extensionPath);
+		if (isSelfExtension && !isCurrentVersion) {
+			library.splice(i, 1);
 		}
-		const index = library.indexOf(folderPath);
-		if (enable) {
-			if (index === -1) {
-				library.push(folderPath);
-			}
-		}
-		else {
-			if (index > -1) {
-				library.splice(index, 1);
-			}
-		}
-		config.update("workspace.library", library, true);
 	}
-}
-
-function updateConfigs() {
-	const sumneko = vscode.workspace.getConfiguration("Lua");
-	// sumneko.update("completion.displayContext", 0, true) // hides the emmylua source from the hover tooltip
-
-	const wowapi = vscode.workspace.getConfiguration("wowAPI");
-	globalstrings.data = require("./data/globalstring/"+wowapi.get("locale")).data;
-
-	// cannot seem to easily fix warnings like these in emmylua wow
-	// Cannot assign `CheckButton|InterfaceOptionsCheckButtonTemplate` to parameter `string|Region`."
-	const diag_disable: string[] | undefined = sumneko.get("diagnostics.disable");
-	const param_mismatch = "param-type-mismatch";
-	if (diag_disable?.indexOf(param_mismatch)) {
-		diag_disable.push(param_mismatch);
-		sumneko.update("diagnostics.disable", diag_disable);
-	}
-}
-
-function onCustomCompletion() {
-	// listen when one of our completion items were committed
-	vscode.commands.registerTextEditorCommand("ketho.wow-api.onCompletion", (editor: vscode.TextEditor) => {
-		const pos = editor.selection.active;
-		const range = editor.document.getWordRangeAtPosition(pos);
-		const word = editor.document.getText(range);
-		// doublecheck if the word was matched properly
-		const isValidWord = enums.data[word] || globalstrings.data[word];
-
-		const config = vscode.workspace.getConfiguration("Lua");
-		const globals: string[] | undefined = config.get("diagnostics.globals");
-		if (isValidWord && globals && globals.indexOf(word)===-1) {
-			globals.push(word);
-			config.update("diagnostics.globals", globals);
+	const index = library.indexOf(folderPath);
+	if (enable) {
+		if (index === -1) {
+			library.push(folderPath);
 		}
-});
+	}
+	else {
+		if (index > -1) {
+			library.splice(index, 1);
+		}
+	}
+	config.update("workspace.library", library, true);
 }
