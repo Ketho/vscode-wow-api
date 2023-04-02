790d9ed351e6349663eb7511da7137ecac472600
James D. Callahan III <darkenelf@gmail.com>
2023-04-02 15:54:35 -0400

    Rename GAME_LOCALE to AceLocale.LocaleCode and sort its members.

--- EmmyLua/API/Libraries/Ace3/AceLocale-3.0.lua
+++ EmmyLua/API/Libraries/Ace3/AceLocale-3.0.lua
@@ -15,7 +15,7 @@ function AceLocale:GetLocale(application, silent) end
 
 ---@paramsig application, locale[, isDefault[, silent]]
 ---@param application string Unique name of addon / module
----@param locale GAME_LOCALE Name of the locale to register, e.g. "enUS", "deDE", etc.
+---@param locale AceLocale.LocaleCode Name of the locale to register, e.g. "enUS", "deDE", etc.
 ---@param isDefault? boolean If this is the default locale being registered. Your addon is written in this language, generally enUS, set this to true (defaults to false)
 ---@param silent? boolean If true, the locale will not issue warnings for missing keys. Must be `true` on the first locale registered. If set to "raw", nils will be returned for unknown keys (no metatable used).
 ---@return table -- Locale Table to add localizations to, or nil if the current locale is not required.
@@ -27,16 +27,16 @@ function AceLocale:NewLocale(application, locale, isDefault, silent) end
 -- AceLocale-3.0 Types
 -- ----------------------------------------------------------------------------
 
----@alias GAME_LOCALE
----|"frFR" French (France)
+---@alias AceLocale.LocaleCode
 ---|"deDE": German (Germany)
 ---|"enGB": English (Great Britain) if returned, can substitute 'enUS' for consistancy
 ---|"enUS": English (America)
----|"itIT": Italian (Italy)
----|"koKR": Korean (Korea) RTL - right-to-left
----|"zhCN": Chinese (China) (simplified) implemented LTR left-to-right in WoW
----|"zhTW": Chinese (Taiwan) (traditional) implemented LTR left-to-right in WoW
----|"ruRU": Russian (Russia)
 ---|"esES": Spanish (Spain)
 ---|"esMX": Spanish (Mexico)
+---|"frFR" French (France)
+---|"itIT": Italian (Italy)
+---|"koKR": Korean (Korea) RTL - right-to-left
 ---|"ptBR": Portuguese (Brazil)
+---|"ruRU": Russian (Russia)
+---|"zhCN": Chinese (China) (simplified) implemented LTR left-to-right in WoW
+---|"zhTW": Chinese (Taiwan) (traditional) implemented LTR left-to-right in WoW
