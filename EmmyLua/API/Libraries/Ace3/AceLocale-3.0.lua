f007f979fca086a8f7f9fb1b1c40f4cb6ebd4131
James D. Callahan III <darkenelf@gmail.com>
2023-04-02 16:04:19 -0400

    Add an AceLocale.Localizations type alias, and set it as the return type for the AceLocale :GetLocale and :NewLocale methods so consumers will have more precision.

--- EmmyLua/API/Libraries/Ace3/AceLocale-3.0.lua
+++ EmmyLua/API/Libraries/Ace3/AceLocale-3.0.lua
@@ -8,7 +8,7 @@ local AceLocale = {}
 
 ---@param application string Unique name of addon / module
 ---@param silent? boolean If true, the locale is optional, silently return nil if it's not found (defaults to false, optional)
----@return table -- The locale table for the current language.
+---@return AceLocale.Localizations -- The locale table for the current language.
 --- ---
 ---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-locale-3-0#title-1)
 function AceLocale:GetLocale(application, silent) end
@@ -18,7 +18,7 @@ function AceLocale:GetLocale(application, silent) end
 ---@param locale AceLocale.LocaleCode Name of the locale to register, e.g. "enUS", "deDE", etc.
 ---@param isDefault? boolean If this is the default locale being registered. Your addon is written in this language, generally enUS, set this to true (defaults to false)
 ---@param silent? boolean If true, the locale will not issue warnings for missing keys. Must be `true` on the first locale registered. If set to "raw", nils will be returned for unknown keys (no metatable used).
----@return table -- Locale Table to add localizations to, or nil if the current locale is not required.
+---@return AceLocale.Localizations -- Locale Table to add localizations to, or nil if the current locale is not required.
 --- ---
 ---[Documentation](https://www.wowace.com/projects/ace3/pages/api/ace-locale-3-0#title-2)
 function AceLocale:NewLocale(application, locale, isDefault, silent) end
@@ -40,3 +40,5 @@ function AceLocale:NewLocale(application, locale, isDefault, silent) end
 ---|"ruRU": Russian (Russia)
 ---|"zhCN": Chinese (China) (simplified) implemented LTR left-to-right in WoW
 ---|"zhTW": Chinese (Taiwan) (traditional) implemented LTR left-to-right in WoW
+
+---@alias AceLocale.Localizations { [string]: string }
