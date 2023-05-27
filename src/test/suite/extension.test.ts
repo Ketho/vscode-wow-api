a8c4cbec735ec0953cd9e863de2195ad4b0c4530
Josh Santos <josh@omnidapps.com>
2023-05-27 19:15:59 +0700

    Fix missing semicolons

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,51 +1,51 @@
 import { expect } from 'chai';
-import * as path from 'path'
+import * as path from 'path';
 import * as vscode from 'vscode';
 import * as wowApiExtension from '../../extension';
 
-const extensionPath = vscode.extensions.getExtension("ketho.wow-api")!.extensionPath
+const extensionPath = vscode.extensions.getExtension("ketho.wow-api")!.extensionPath;
 
 suite('Config Tests', () => {
     vscode.window.showInformationMessage('Start all tests.');
 
     test('Default Behaviour - It can set both WoW API and FrameXML libraries', async () => {
-        await vscode.workspace.getConfiguration("Lua").update("workspace.library", undefined, true)
-        await wowApiExtension.setExternalLibrary("API", true)
-        await wowApiExtension.setExternalLibrary("Optional", true)
+        await vscode.workspace.getConfiguration("Lua").update("workspace.library", undefined, true);
+        await wowApiExtension.setExternalLibrary("API", true);
+        await wowApiExtension.setExternalLibrary("Optional", true);
 
-        const luaConfig = vscode.workspace.getConfiguration("Lua")
+        const luaConfig = vscode.workspace.getConfiguration("Lua");
 
         expect(luaConfig.get("workspace.library")).to.have.all.members([
             path.join(extensionPath, "/EmmyLua/Optional"),
             path.join(extensionPath, "/EmmyLua/API")
-        ])
+        ]);
     });
 
     test('It can set only WoW API libraries', async () => {
-        await vscode.workspace.getConfiguration("Lua").update("workspace.library", undefined, true)
+        await vscode.workspace.getConfiguration("Lua").update("workspace.library", undefined, true);
 
-        await wowApiExtension.setExternalLibrary("API", true)
-        await wowApiExtension.setExternalLibrary("Optional", false)
+        await wowApiExtension.setExternalLibrary("API", true);
+        await wowApiExtension.setExternalLibrary("Optional", false);
 
-        const luaConfig = vscode.workspace.getConfiguration("Lua")
+        const luaConfig = vscode.workspace.getConfiguration("Lua");
 
         expect(luaConfig.get("workspace.library")).to.have.all.members([
             path.join(extensionPath, "/EmmyLua/API")
-        ])
+        ]);
     });
 
     test('It can set additional libraries', async () => {
-        await vscode.workspace.getConfiguration("Lua").update("workspace.library", [path.resolve("/foo/bar")], true)
+        await vscode.workspace.getConfiguration("Lua").update("workspace.library", [path.resolve("/foo/bar")], true);
 
-        await wowApiExtension.setExternalLibrary("API", true)
-        await wowApiExtension.setExternalLibrary("Optional", true)
+        await wowApiExtension.setExternalLibrary("API", true);
+        await wowApiExtension.setExternalLibrary("Optional", true);
 
-        const luaConfig = vscode.workspace.getConfiguration("Lua")
+        const luaConfig = vscode.workspace.getConfiguration("Lua");
 
         expect(luaConfig.get("workspace.library")).to.have.all.members([
             path.resolve("/foo/bar"),
             path.join(extensionPath, "/EmmyLua/API"),
             path.join(extensionPath, "/EmmyLua/Optional")
-        ])
+        ]);
     });
 });
