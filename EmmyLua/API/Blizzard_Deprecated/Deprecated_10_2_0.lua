8cc0e6c0d37945c635bbae39b3a85c299f89c87e
Ketho <sghs17@outlook.com>
2023-11-21 23:01:16 +0100

    Update deprecated addon api

--- EmmyLua/API/Blizzard_Deprecated/Deprecated_10_2_0.lua
+++ EmmyLua/API/Blizzard_Deprecated/Deprecated_10_2_0.lua
@@ -4,28 +4,37 @@
 function GetCVarInfo() end
 
 ---@deprecated
---- Deprecated by [C_CVar.GetCVarInfo](https://warcraft.wiki.gg/wiki/API_C_CVar.GetCVarInfo)
-function GetCVarInfo() end
-
----@deprecated
 --- Deprecated by [C_AddOns.EnableAddOn](https://warcraft.wiki.gg/wiki/API_C_AddOns.EnableAddOn)
-function EnableAddOn() end
+---@param name string|number
+---@param character? string|boolean
+function EnableAddOn(name, character) end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.DisableAddOn](https://warcraft.wiki.gg/wiki/API_C_AddOns.DisableAddOn)
-function DisableAddOn() end
+---@param name string|number
+---@param character? string|boolean
+function DisableAddOn(name, character) end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.GetAddOnEnableState](https://warcraft.wiki.gg/wiki/API_C_AddOns.GetAddOnEnableState)
-function GetAddOnEnableState() end
+---@param character? string
+---@param name string|number
+---@return number enabledState
+function GetAddOnEnableState(character, name) end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.LoadAddOn](https://warcraft.wiki.gg/wiki/API_C_AddOns.LoadAddOn)
-function LoadAddOn() end
+---@param name string|number
+---@return boolean loaded
+---@return string? reason
+function LoadAddOn(name) end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.IsAddOnLoaded](https://warcraft.wiki.gg/wiki/API_C_AddOns.IsAddOnLoaded)
-function IsAddOnLoaded() end
+---@param name string|number
+---@return boolean loaded
+---@return boolean finished
+function IsAddOnLoaded(name) end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.EnableAllAddOns](https://warcraft.wiki.gg/wiki/API_C_AddOns.EnableAllAddOns)
@@ -37,18 +46,31 @@ function DisableAllAddOns() end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.GetAddOnInfo](https://warcraft.wiki.gg/wiki/API_C_AddOns.GetAddOnInfo)
-function GetAddOnInfo() end
+---@param name string|number
+---@return string name
+---@return string title
+---@return string notes
+---@return boolean loadable
+---@return string reason
+---@return string security
+---@return boolean newVersion
+function GetAddOnInfo(name) end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.GetAddOnDependencies](https://warcraft.wiki.gg/wiki/API_C_AddOns.GetAddOnDependencies)
-function GetAddOnDependencies() end
+---@param name string|number
+---@return ... string
+function GetAddOnDependencies(name) end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.GetAddOnOptionalDependencies](https://warcraft.wiki.gg/wiki/API_C_AddOns.GetAddOnOptionalDependencies)
-function GetAddOnOptionalDependencies() end
+---@param name string|number
+---@return ... string
+function GetAddOnOptionalDependencies(name) end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.GetNumAddOns](https://warcraft.wiki.gg/wiki/API_C_AddOns.GetNumAddOns)
+---@return number count
 function GetNumAddOns() end
 
 ---@deprecated
@@ -65,12 +87,15 @@ function ResetDisabledAddOns() end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.IsAddonVersionCheckEnabled](https://warcraft.wiki.gg/wiki/API_C_AddOns.IsAddonVersionCheckEnabled)
+---@return boolean
 function IsAddonVersionCheckEnabled() end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.SetAddonVersionCheck](https://warcraft.wiki.gg/wiki/API_C_AddOns.SetAddonVersionCheck)
-function SetAddonVersionCheck() end
+function SetAddonVersionCheck(boolean) end
 
 ---@deprecated
 --- Deprecated by [C_AddOns.IsAddOnLoadOnDemand](https://warcraft.wiki.gg/wiki/API_C_AddOns.IsAddOnLoadOnDemand)
-function IsAddOnLoadOnDemand() end
+---@param name string|number
+---@return boolean loadDemand
+function IsAddOnLoadOnDemand(name) end
