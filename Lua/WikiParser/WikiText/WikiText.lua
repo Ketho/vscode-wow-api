6c201b75067049daaf7e72d0e09de040c46a7580
Ketho <sghs17@outlook.com>
2024-05-28 10:56:34 +0200

    warcraftwiki doesnt like me

--- Lua/WikiParser/WikiText/WikiText.lua
+++ Lua/WikiParser/WikiText/WikiText.lua
@@ -9,7 +9,7 @@ local m = {}
 function m:SaveExport()
 	local url = "https://warcraft.wiki.gg/wiki/Special:Export"
 	local requestBody = "pages=World_of_Warcraft_API&curonly=1"
-	Util:DownloadFilePost(INPUT, url, requestBody)
+	Util:DownloadFilePost(INPUT, url, requestBody, 300)
 end
 
 local symbols = {
