6aa55af1a370010b53bc7d262c02c8af6ef0baf8
Ketho <sghs17@outlook.com>
2024-07-18 21:33:22 +0200

    Test nonexisting template

--- Lua/Tests/FrameXML/Templates.lua
+++ Lua/Tests/FrameXML/Templates.lua
@@ -3,3 +3,6 @@
 -- single templates
 local frame_backdrop = CreateFrame("Button", nil, UIParent, "BackdropTemplate")
 local bd = frame_backdrop:GetBackdrop()
+
+local notemplate = CreateFrame("Frame", nil, UIParent, "NonexistentTemplate")
+notemplate.bar = 123
