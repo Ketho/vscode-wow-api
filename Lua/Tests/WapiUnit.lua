57333367a40943cc14d6e562b9270173e6160e3e
Ketho <sghs17@outlook.com>
2024-07-31 21:13:06 +0200

    wowunit already exists

--- Lua/Tests/WapiUnit.lua
+++ Lua/Tests/WapiUnit.lua
@@ -1,36 +1,36 @@
-local WowUnit = {}
+local WapiUnit = {}
 
 local msg = "expected %s, got %s"
 
-function WowUnit:assert_true(v)
+function WapiUnit:assert_true(v)
     assert(type(v) == "boolean", msg:format("boolean", type(v)))
     assert(v == true, msg:format("true", v))
 end
 
-function WowUnit:assert_false(v)
+function WapiUnit:assert_false(v)
     assert(type(v) == "boolean", msg:format("boolean", type(v)))
     assert(v == false, msg:format("false", v))
 end
 
-function WowUnit:assert_nil(v)
+function WapiUnit:assert_nil(v)
     assert(type(v) == "nil", msg:format("nil", type(v)))
     assert(v == nil, msg:format("nil", v))
 end
 
-function WowUnit:assert_number(v, expected)
+function WapiUnit:assert_number(v, expected)
     assert(type(v) == "number", msg:format("number", type(v)))
     if expected then
         assert(v == expected, msg:format(expected, v))
     end
 end
 
-function WowUnit:assert_string(v, expected)
+function WapiUnit:assert_string(v, expected)
     assert(type(v) == "string", msg:format("string", type(v)))
     if expected then
         assert(v == expected, msg:format(expected, v))
     end
 end
 
--- WowUnit:assert_string(124, "hello")
+-- WapiUnit:assert_string(124, "hello")
 
-return WowUnit
+return WapiUnit
