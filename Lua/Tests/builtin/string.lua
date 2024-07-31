bd2287d182d1962029dce2b0d219da62b5bedb3c
Ketho <sghs17@outlook.com>
2024-07-31 21:23:58 +0200

    replace table module

--- Lua/Tests/builtin/string.lua
+++ Lua/Tests/builtin/string.lua
@@ -1,7 +1,4 @@
----@diagnostic disable:unused-local
-
 local str2 = format("%s %d", "greedisgood", 999)
-local num1 = bit.bor(0x1, 0x2)
 
 --- string.trim / strtrim
 local trim1 = strtrim(" hello world ")
