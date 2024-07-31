e1ac4021233af642cfdbed21800dda23716367c8
Ketho <sghs17@outlook.com>
2024-07-31 20:52:16 +0200

    wowunit

--- Lua/Tests/WowUnit.lua
+++ Lua/Tests/WowUnit.lua
@@ -1,34 +1,36 @@
-wowapi_tests = {}
+local WowUnit = {}
 
 local msg = "expected %s, got %s"
 
-function wowapi_tests:assert_true(v)
+function WowUnit:assert_true(v)
     assert(type(v) == "boolean", msg:format("boolean", type(v)))
     assert(v == true, msg:format("true", v))
 end
 
-function wowapi_tests:assert_false(v)
+function WowUnit:assert_false(v)
     assert(type(v) == "boolean", msg:format("boolean", type(v)))
     assert(v == false, msg:format("false", v))
 end
 
-function wowapi_tests:assert_nil(v)
+function WowUnit:assert_nil(v)
     assert(type(v) == "nil", msg:format("nil", type(v)))
     assert(v == nil, msg:format("nil", v))
 end
 
-function wowapi_tests:assert_number(v, expected)
+function WowUnit:assert_number(v, expected)
     assert(type(v) == "number", msg:format("number", type(v)))
     if expected then
         assert(v == expected, msg:format(expected, v))
     end
 end
 
-function wowapi_tests:assert_string(v, expected)
+function WowUnit:assert_string(v, expected)
     assert(type(v) == "string", msg:format("string", type(v)))
     if expected then
         assert(v == expected, msg:format(expected, v))
     end
 end
 
--- wowapi_tests:assert_string(124, "hello")
+-- WowUnit:assert_string(124, "hello")
+
+return WowUnit
