9d89a8365912c81a546abb5c1c8b4fe06fbcb6f4
Ketho <sghs17@outlook.com>
2025-04-01 07:08:23 +0200

    Remove old deprecations

--- Annotations/Interface/Blizzard_Deprecated/Deprecated_10_1_0.lua
+++
@@ -1,7 +0,0 @@
----@meta _
----@deprecated
----Deprecated by [C_AddOns.GetAddOnMetadata](https://warcraft.wiki.gg/wiki/API_C_AddOns.GetAddOnMetadata)
----@param name uiAddon
----@param variable string
----@return string value
-function GetAddOnMetadata(name, variable) end
--- Annotations/Interface/Blizzard_Deprecated/Deprecated_10_1_5.lua
+++
@@ -1,6 +0,0 @@
----@meta _
----@deprecated
----Deprecated by [C_SpecializationInfo.GetClassIDFromSpecID](https://warcraft.wiki.gg/wiki/API_C_SpecializationInfo.GetClassIDFromSpecID)
----@param specID number
----@return number? classID
-function GetClassIDFromSpecID(specID) end
