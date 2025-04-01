1135f063b7b4d635f2f0192fc0f5f1129db33ae7
Ketho <sghs17@outlook.com>
2025-04-01 06:41:31 +0200

    Purge old systems

--- Annotations/Interface/Blizzard_APIDocumentationGenerated/GameModeManagerDocumentation.lua
+++
@@ -1,6 +0,0 @@
----@meta _
-C_GameModeManager = {}
-
----[Documentation](https://warcraft.wiki.gg/wiki/API_C_GameModeManager.GetCurrentGameMode)
----@return Enum.GameMode gameMode
-function C_GameModeManager.GetCurrentGameMode() end
