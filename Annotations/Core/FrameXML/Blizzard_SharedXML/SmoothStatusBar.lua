c5b0c28dad19fe703ee80aaa9265ef6a7a72c114
Ketho <sghs17@outlook.com>
2025-04-11 00:54:20 +0200

    Move framexml folders again Copypaste instead of submoduling FramexmlAnnotations for manual editing

--- Annotations/Core/FrameXML/Blizzard_SharedXML/SmoothStatusBar.lua
+++
@@ -1,5 +0,0 @@
----@meta _
-SmoothStatusBarMixin = {}
-function SmoothStatusBarMixin:ResetSmoothedValue(value) end
-function SmoothStatusBarMixin:SetSmoothedValue(value) end
-function SmoothStatusBarMixin:SetMinMaxSmoothedValue(min, max) end
