c6749a5c294ec38a0de9f0aba881016cdead4383
Ketho <sghs17@outlook.com>
2025-08-18 22:12:36 +0200

    update to node 24.6.0 from 18.19.1

--- setup.sh
+++ setup.sh
@@ -3,10 +3,6 @@ set -e
 
 sudo apt update
 
-# npm
-sudo apt install npm -y
-npm install
-
 # venv
 sudo apt install python3-pip python3-venv -y
 python3 -m venv .venv
@@ -18,8 +14,14 @@ pip install git+https://github.com/luarocks/hererocks
 hererocks .lua -l latest -r latest
 source .lua/bin/activate
 
-# modules
+# lua modules
 sudo apt install libssl-dev -y
 luarocks build
+
+# node https://nodejs.org/en/download/current
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 24
+npm install
 
 echo "Setup complete for vscode-wow-api"
