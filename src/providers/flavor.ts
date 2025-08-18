8a24cb11144f9259f7b9b6953dace5fa6591b040
Ketho <sghs17@outlook.com>
2025-08-18 22:31:15 +0200

    Update game type tooltip for mists

--- src/providers/flavor.ts
+++ src/providers/flavor.ts
@@ -7,10 +7,10 @@ export function getHover(name: string) {
 	const el = data[name];
 	const array = [
 		(el & 0x1) > 0 && "Mainline",
-		(el & 0x4) > 0 && "Cata",
+		(el & 0x4) > 0 && "Mists",
 		(el & 0x2) > 0 && "Vanilla",
 	].filter(Boolean);
-	const md = new vscode.MarkdownString("Flavors: `"+array.join(", ")+"`\n");
+	const md = new vscode.MarkdownString("Game Types: `"+array.join(", ")+"`\n");
 	const item = new vscode.Hover(md);
 	return item;
 }
