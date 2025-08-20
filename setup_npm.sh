3ba6a13aedfd7e918173dd155c30793b9ad321d0
Ketho <sghs17@outlook.com>
2025-08-20 02:04:47 +0200

    Update setup_npm.sh

--- setup_npm.sh
+++ setup_npm.sh
@@ -6,5 +6,3 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 24
 npm install
-
-echo "Setup complete for developing the VS Code extension"
