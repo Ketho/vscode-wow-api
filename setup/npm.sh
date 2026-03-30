ca353bb571af9057832c43f7f04732912ef23c58
Ketho <sghs17@outlook.com>
2026-03-30 05:13:41 +0200

    Update setup readme

--- setup/npm.sh
+++ setup/npm.sh
@@ -2,7 +2,7 @@
 set -e
 
 # https://nodejs.org/en/download/current
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 24
 npm install
